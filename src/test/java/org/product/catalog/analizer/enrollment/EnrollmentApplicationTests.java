7f1abf705c321f742ecb70dcf4427b33cb33bd74
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-18 18:52:52 +0300

    Add project structure, source and properties file.

---
+++ src/test/java/org/product/catalog/analizer/enrollment/EnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.product.catalog.analizer.enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
