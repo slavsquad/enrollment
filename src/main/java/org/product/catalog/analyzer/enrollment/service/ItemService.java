1b169c260bea21b88496a55f7835e6b65bae1f7e
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-19 21:03:07 +0300

    Add import item service.

---
+++ src/main/java/org/product/catalog/analyzer/enrollment/service/ItemService.java
@@ -0,0 +1,7 @@
+package org.product.catalog.analyzer.enrollment.service;
+
+import org.product.catalog.analyzer.enrollment.dto.Item;
+
+public interface ItemService {
+    void addItem(Item item);
+}
