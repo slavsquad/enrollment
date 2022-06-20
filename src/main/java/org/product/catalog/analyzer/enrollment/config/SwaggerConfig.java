924cafc89096daa2f5673c7180e14eff7fd25014
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-21 00:34:40 +0300

    Improve swagger request.

--- src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
+++ src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
@@ -3,13 +3,12 @@ package org.product.catalog.analyzer.enrollment.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Set;
 
 @Configuration
 @EnableSwagger2
@@ -18,11 +17,10 @@ public class SwaggerConfig {
     @Bean
     public Docket createDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .protocols(Set.of("https", "http"))
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.product.catalog.analyzer.enrollment"))
+                .paths(PathSelectors.any())
                 .build();
     }
 
