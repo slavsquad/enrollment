bbcb1d68c64eaa5697c4e946df053595fc37fd13
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-20 19:32:03 +0300

    Remove api path redirect swagger config.

--- src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
+++ src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
@@ -26,22 +26,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
     private String serviceVersion;
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/api/v2/api-docs",
-                "/v2/api-docs");
-        registry.addRedirectViewController("/api/swagger-resources/configuration/ui",
-                "/swagger-resources/configuration/ui");
-        registry.addRedirectViewController("/api/swagger-resources/configuration/security",
-                "/swagger-resources/configuration/security");
-        registry.addRedirectViewController("/api/swagger-resources",
-                "/swagger-resources");
-    }
-
-    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/api/swagger-ui.html**")
+        registry.addResourceHandler("/swagger-ui.html**")
                 .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
-        registry.addResourceHandler("/api/webjars/**")
+        registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
