c959841d29a959a7a97bfa532ec984366448566a
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-20 20:34:39 +0300

    Remove warnings from swagger app.

--- src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
+++ src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
@@ -9,12 +9,17 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Slf4j
 @EnableWebMvc
@@ -26,16 +31,37 @@ public class SwaggerConfig implements WebMvcConfigurer {
     private String serviceVersion;
 
     @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api/v2/api-docs",
+                "/v2/api-docs");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/ui",
+                "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/security",
+                "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/api/swagger-resources",
+                "/swagger-resources");
+    }
+
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html**")
+        registry.addResourceHandler("/api/swagger-ui.html**")
                 .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
-        registry.addResourceHandler("/webjars/**")
+        registry.addResourceHandler("/api/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Bean
     public Docket api() {
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(Collections.singletonList(new ParameterBuilder()
+                        .name("Authorization")
+                        .description("Токен идентификации пользователя")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .required(true)
+                        .scalarExample("Bearer <token>")
+                        .build()))
                 .select()
                 .apis(RequestHandlerSelectors
                         .basePackage("org.product.catalog.analyzer.enrollment"))
@@ -43,14 +69,12 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .build()
                 .apiInfo(apiEndPointsInfo())
                 .pathMapping("/");
-        log.info("Docket: {}", docket);
-        return docket;
     }
 
     private ApiInfo apiEndPointsInfo() {
         return new ApiInfoBuilder()
-                .title("")
-                .description("")
+                .title("Swagger API Doc")
+                .description("More description about the API")
                 .version(serviceVersion)
                 .build();
     }
