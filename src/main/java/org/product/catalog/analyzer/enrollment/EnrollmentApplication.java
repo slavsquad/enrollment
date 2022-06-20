32ad2852f8289d4a5238e2181421c619e4cb6763
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-20 23:38:13 +0300

    Increased swagger version.

--- src/main/java/org/product/catalog/analyzer/enrollment/EnrollmentApplication.java
+++ src/main/java/org/product/catalog/analyzer/enrollment/EnrollmentApplication.java
@@ -3,7 +3,7 @@ package org.product.catalog.analyzer.enrollment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.product.catalog.analyzer.enrollment"})
+@SpringBootApplication
 public class EnrollmentApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
+++ src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
@@ -1,83 +1,36 @@
 package org.product.catalog.analyzer.enrollment.config;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
+import java.util.Set;
 
-@Slf4j
-@EnableWebMvc
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer {
-
-    @Value("${springfox.documentation.service-version}")
-    private String serviceVersion;
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/api/v2/api-docs",
-                "/v2/api-docs");
-        registry.addRedirectViewController("/api/swagger-resources/configuration/ui",
-                "/swagger-resources/configuration/ui");
-        registry.addRedirectViewController("/api/swagger-resources/configuration/security",
-                "/swagger-resources/configuration/security");
-        registry.addRedirectViewController("/api/swagger-resources",
-                "/swagger-resources");
-    }
-
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/api/swagger-ui.html**")
-                .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
-        registry.addResourceHandler("/api/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
+    public Docket createDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .globalOperationParameters(Collections.singletonList(new ParameterBuilder()
-                        .name("Authorization")
-                        .description("Токен идентификации пользователя")
-                        .modelRef(new ModelRef("string"))
-                        .parameterType("header")
-                        .required(true)
-                        .scalarExample("Bearer <token>")
-                        .build()))
+                .useDefaultResponseMessages(false)
+                .protocols(Set.of("https", "http"))
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors
-                        .basePackage("org.product.catalog.analyzer.enrollment"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiEndPointsInfo())
-                .pathMapping("/");
-    }
-
-    private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder()
-                .title("Swagger API Doc")
-                .description("More description about the API")
-                .version(serviceVersion)
+                .apis(RequestHandlerSelectors.basePackage("org.product.catalog.analyzer.enrollment"))
                 .build();
     }
 
-
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Product Catalog Swagger")
+                .description("Automated JSON API documentation for API's built with Spring")
+                .version("1.0.0")
+                .build();
+    }
 }
