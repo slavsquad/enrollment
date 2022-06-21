7cc4546501a3a8e9511bc5c6f0d9b161ee4dc9c7
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-21 23:16:28 +0300

    Category price changed from null to 0.

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -4,6 +4,6 @@ CREATE TABLE item
     type character varying(50) NOT NULL,
     name character varying(50) NOT NULL,
     parent_id uuid,
-    price integer,
+    price integer NOT NULL,
     update_date timestamp without time zone
 );
