63563112be30775e6134b8540204670ccad8c5a5
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-21 13:40:19 +0300

    Create custom valid argument exception.

---
+++ src/main/java/org/product/catalog/analyzer/enrollment/validation/exception/ArgumentNotValidException.java
@@ -0,0 +1,8 @@
+package org.product.catalog.analyzer.enrollment.validation.exception;
+
+public class ArgumentNotValidException extends Exception{
+
+    public ArgumentNotValidException(String message) {
+        super(message);
+    }
+}
