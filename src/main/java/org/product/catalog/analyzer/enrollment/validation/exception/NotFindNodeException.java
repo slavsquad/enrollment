5332edd6be284f424ce4b9ef63b8a1603ddc5d9a
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-23 01:06:08 +0300

    Created not find node exception.

---
+++ src/main/java/org/product/catalog/analyzer/enrollment/validation/exception/NotFindNodeException.java
@@ -0,0 +1,8 @@
+package org.product.catalog.analyzer.enrollment.validation.exception;
+
+public class NotFindNodeException extends Exception{
+
+    public NotFindNodeException(String message) {
+        super(message);
+    }
+}
