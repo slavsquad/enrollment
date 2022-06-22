7a3ea8648bfb6c422d5f8e0a5c3dc7ee86a6eed9
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-23 00:54:06 +0300

    Added methodology of calc average price.

--- src/main/java/org/product/catalog/analyzer/enrollment/repository/NodeRepository.java
+++ src/main/java/org/product/catalog/analyzer/enrollment/repository/NodeRepository.java
@@ -3,12 +3,11 @@ package org.product.catalog.analyzer.enrollment.repository;
 import org.product.catalog.analyzer.enrollment.dto.Node;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 public interface NodeRepository {
-    Optional<Node> findById(UUID id);
+    Node findById(UUID id);
     int save(Node node);
     int saveAll(List<Node> nodes);
     Set<UUID> findCategoryAllId();
--- src/main/java/org/product/catalog/analyzer/enrollment/repository/NodeRepositoryImpl.java
+++ src/main/java/org/product/catalog/analyzer/enrollment/repository/NodeRepositoryImpl.java
@@ -3,6 +3,7 @@ package org.product.catalog.analyzer.enrollment.repository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.product.catalog.analyzer.enrollment.dto.Node;
+import org.product.catalog.analyzer.enrollment.dto.NodeType;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -10,21 +11,93 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
+
 @Slf4j
 @Repository
 @AllArgsConstructor
 public class NodeRepositoryImpl implements NodeRepository {
+    final private static byte WHITE = 0;
+    final private static byte GRAY = -1;
+    final private static byte BLACK = 1;
     private final JdbcTemplate jdbcTemplate;
 
     @Override
     @Transactional
-    public Optional<Node> findById(UUID id) {
-        return findByIdTx(id);
+    public Node findById(UUID id) {
+        log.info("Start find node by id:{}", id);
+        final Node result = findByIdTx(id);
+        if (result == null) return result;
+        if (NodeType.OFFER.equals(result.getType())) return result;
+        Deque<Node> stack = new ArrayDeque<>();
+        Map<UUID, Byte> colors = new HashMap<>();
+        stack.push(result);
+        colors.put(result.getId(), WHITE);
+        while (!stack.isEmpty()) {
+            Node parent = stack.pop();
+            switch (colors.get(parent.getId())) {
+                case WHITE -> {
+                    colors.put(parent.getId(), GRAY);
+                    stack.push(parent);
+                    List<Node> children = findAllChildrenByIdTx(parent.getId());
+                    parent.setChildren(children);
+                    children.stream()
+                            .filter(node -> NodeType.CATEGORY.equals(node.getType()))
+                            .forEach(child -> {
+                                stack.push(child);
+                                colors.put(child.getId(), WHITE);
+                            });
+                }
+                case GRAY -> {
+                    colors.put(parent.getId(), BLACK);
+                    if (parent.getChildren().isEmpty()) break;
+                    final int sum = parent.getChildren()
+                            .stream()
+                            .mapToInt(Node::getSum)
+                            .sum();
+                    final int offerCount = parent.getChildren()
+                            .stream()
+                            .mapToInt(Node::getOfferCount)
+                            .sum();
+                    log.info("parent with ID: {} Sum -> {} OfferCount -> {}", parent.getParentId(), sum, offerCount);
+                    parent.setSum(sum);
+                    parent.setOfferCount(offerCount);
+                    int price = sum;
+                    if (offerCount != 0) {
+                        price /= offerCount;
+                    }
+                    parent.setPrice(price);
+                }
+            }
+        }
+        log.info("Finish find node by id:{}", id);
+        return result;
     }
+
+    private List<Node> findAllChildrenByIdTx(UUID id) {
+        log.info("Start find children by node id:{}", id);
+        return jdbcTemplate.query("""
+                        SELECT id, type, name, parent_id, price, to_char(date, 'yyyy-mm-dd hh24:mi:ss') as date
+                           FROM node
+                           WHERE parent_id = ?::uuid""",
+                (rs, rowNum) -> new Node(
+                        UUID.fromString(rs.getString("id")),
+                        rs.getString("type"),
+                        rs.getString("name"),
+                        rs.getString("parent_id") == null ? null : UUID.fromString(rs.getString("parent_id")),
+                        rs.getObject("price", Integer.class),
+                        rs.getTimestamp("date"),
+                        null,
+                        NodeType.OFFER.equals(rs.getString("type")) ? 1 : 0,
+                        rs.getInt("price")
+                ),
+                id);
+    }
+
 
     @Override
     @Transactional
     public int save(Node node) {
+        log.info("Start save node:{}", node.getId());
         return saveTx(node);
     }
 
@@ -40,29 +113,32 @@ public class NodeRepositoryImpl implements NodeRepository {
         return count;
     }
 
-    private Optional<Node> findByIdTx(UUID id){
+    private Node findByIdTx(UUID id) {
         try {
             return jdbcTemplate.queryForObject("""
                             SELECT id, type, name, parent_id, price, to_char(date, 'yyyy-mm-dd hh24:mi:ss') as date
                                FROM node
                                WHERE id = ?::uuid""",
-                    (rs, rowNum) -> Optional.of(new Node(
+                    (rs, rowNum) -> new Node(
                             UUID.fromString(rs.getString("id")),
                             rs.getString("type"),
                             rs.getString("name"),
                             rs.getString("parent_id") == null ? null : UUID.fromString(rs.getString("parent_id")),
-                            rs.getInt("price"),
+                            rs.getObject("price", Integer.class),
                             rs.getTimestamp("date"),
-                            null
-                    )),
+                            null,
+                            NodeType.OFFER.equals(rs.getString("type")) ? 1 : 0,
+                            rs.getInt("price")
+                    ),
                     id);
         } catch (EmptyResultDataAccessException e) {
             log.info("Node with id:{} didn't find!", id);
-            return Optional.empty();
+            return null;
         }
     }
+
     private int saveTx(Node node) {
-        int count = jdbcTemplate.update("""
+        final int count = jdbcTemplate.update("""
                         INSERT INTO 
                             node (
                                 id, 
@@ -91,11 +167,12 @@ public class NodeRepositoryImpl implements NodeRepository {
                 node.getDate(),
                 node.getParentId(),
                 node.getPrice());
-        log.info("Save: {} is successfully complete!", node);
+        log.info("Save node with ID: {} is successfully complete!", node.getId());
         return count;
     }
 
     @Override
+    @Transactional
     public Set<UUID> findCategoryAllId() {
         return new HashSet<>(jdbcTemplate.queryForList("""
                         SELECT 
@@ -103,7 +180,8 @@ public class NodeRepositoryImpl implements NodeRepository {
                         FROM 
                             node 
                         WHERE 
-                            type = 'CATEGORY'""",
-                UUID.class));
+                            type = ?""",
+                UUID.class,
+                NodeType.CATEGORY));
     }
 }
