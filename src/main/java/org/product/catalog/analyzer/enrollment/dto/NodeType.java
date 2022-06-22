d499178c1a6973c226f405f29b80e095bbaaf545
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-22 21:56:28 +0300

    Added constant node type.

---
+++ src/main/java/org/product/catalog/analyzer/enrollment/dto/NodeType.java
@@ -0,0 +1,8 @@
+package org.product.catalog.analyzer.enrollment.dto;
+
+public class NodeType {
+    public static final String CATEGORY = "CATEGORY";
+    public static final String OFFER = "OFFER";
+
+
+}
