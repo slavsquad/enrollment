1989c9d6f3a067af5822a77f047740bc2918ccc0
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-23 09:32:58 +0300

    Added java documentation into application configuration classes.

--- src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
+++ src/main/java/org/product/catalog/analyzer/enrollment/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package org.product.catalog.analyzer.enrollment.config;
 
+import org.product.catalog.analyzer.enrollment.validation.exception.ArgumentNotValidException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -10,10 +11,24 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * Конфигурационный класс, предназначен для настройки автоматического описания
+ * методов взаимодействия с приложением.
+ *
+ * @author Stepanenko Stanislav
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+
+    /**
+     * Метод создает конструктор, который служит для дальнейшего построения интерфейса,
+     * инструмента автоматического описания методов взаимодействия с приложением(Springfox framework).
+     *
+     * @return - конструктор для построения интерфеса, инструмента автоматического описания
+     * методов взаимодействия с приложением(Springfox framework)
+     */
     @Bean
     public Docket createDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -24,10 +39,17 @@ public class SwaggerConfig {
                 .build();
     }
 
+    /**
+     * Приватный метод создает информационное отображение, инструмента автоматического описания
+     * методов взаимодействия с приложением(Springfox framework).
+     *
+     * @return - информационное отображение инструмента, автоматического описания
+     * методов взаимодействия с приложением(Springfox framework)
+     */
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Product Catalog Swagger")
-                .description("Automated JSON API documentation for API's built with Spring")
+                .title("Mega Market Open API")
+                .description("Вступительное задание в Летнюю Школу Бэкенд Разработки Яндекса 2022")
                 .version("1.0.0")
                 .build();
     }
