16f3fcd537b71ab7356f61532507fa0075085a7d
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-25 23:37:29 +0300

    Refactor main test file.

--- src/test/java/org/product/catalog/analyzer/enrollment/EnrollmentApplicationTests.java
+++ src/test/java/org/product/catalog/analyzer/enrollment/EnrollmentApplicationTests.java
@@ -1,12 +1,18 @@
 package org.product.catalog.analyzer.enrollment;
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY;
+
 @SpringBootTest
+@AutoConfigureEmbeddedDatabase(provider = ZONKY)
 class EnrollmentApplicationTests {
 
-/*	@Test
+
+	@Test
 	void contextLoads() {
-	}*/
+	}
 
 }
