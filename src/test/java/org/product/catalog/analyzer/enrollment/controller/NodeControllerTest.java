76c8fff820a333160e96180850a4990a295152f0
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-25 23:37:57 +0300

    Added integration test for node controller.

---
+++ src/test/java/org/product/catalog/analyzer/enrollment/controller/NodeControllerTest.java
@@ -0,0 +1,100 @@
+package org.product.catalog.analyzer.enrollment.controller;
+
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@AutoConfigureEmbeddedDatabase(provider = ZONKY)
+class NodeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void importNodes() throws Exception {
+        String request = """
+				   {
+				"items": [
+				           {
+				               "type": "CATEGORY",
+				               "name": "Товары",
+				               "id": "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1",
+				               "parentId": null
+				           },
+				           {
+				               "type": "CATEGORY",
+				               "name": "Смартфоны",
+				               "id": "d515e43f-f3f6-4471-bb77-6b455017a2d2",
+				               "parentId": "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1"
+				           },
+				           {
+				               "type": "OFFER",
+				               "name": "jPhone 13",
+				               "id": "863e1a7a-1304-42ae-943b-179184c077e3",
+				               "parentId": "d515e43f-f3f6-4471-bb77-6b455017a2d2",
+				               "price": 79999
+				           },
+				           {
+				               "type": "OFFER",
+				               "name": "Xomiа Readme 10",
+				               "id": "b1d8fd7d-2ae3-47d5-b2f9-0f094af800d4",
+				               "parentId": "d515e43f-f3f6-4471-bb77-6b455017a2d2",
+				               "price": 59999
+				           },
+				           {
+				               "type": "CATEGORY",
+				               "name": "Телевизоры",
+				               "id": "1cc0129a-2bfe-474c-9ee6-d435bf5fc8f2",
+				               "parentId": "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1"
+				           },
+				           {
+				               "type": "OFFER",
+				               "name": "Samson 70\\" LED UHD Smart",
+				               "id": "98883e8f-0507-482f-bce2-2fb306cf6483",
+				               "parentId": "1cc0129a-2bfe-474c-9ee6-d435bf5fc8f2",
+				               "price": 32999
+				           },
+				           {
+				               "type": "OFFER",
+				               "name": "Phyllis 50\\" LED UHD Smarter",
+				               "id": "74b81fda-9cdc-4b63-8927-c978afed5cf4",
+				               "parentId": "1cc0129a-2bfe-474c-9ee6-d435bf5fc8f2",
+				               "price": 49999
+				           },
+				           {
+				               "type": "OFFER",
+				               "name": "Goldstar 65\\" LED UHD LOL Very Smart",
+				               "id": "73bc3b36-02d1-4245-ab35-3106c9ee1c65",
+				               "parentId": "1cc0129a-2bfe-474c-9ee6-d435bf5fc8f2",
+				               "price": 69999
+				           }
+				       ],
+				       "updateDate": "1999-09-09T23:00:00.000Z"
+				   }""";
+        mockMvc.perform(post("/imports")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteNode() {
+    }
+
+    @Test
+    void getNode() {
+    }
+}
