364dd050300a2fc2633a7bfd6d6ecb3d8181d357
stepanenkostas87 <stepanenkostas87@yandex.ru>
2022-06-26 00:03:01 +0300

    Refactor integration test.

--- src/test/java/org/product/catalog/analyzer/enrollment/controller/NodeControllerTest.java
+++ src/test/java/org/product/catalog/analyzer/enrollment/controller/NodeControllerTest.java
@@ -2,11 +2,10 @@ package org.product.catalog.analyzer.enrollment.controller;
 
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY;
@@ -14,15 +13,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
 @AutoConfigureMockMvc
-@RunWith(SpringRunner.class)
 @AutoConfigureEmbeddedDatabase(provider = ZONKY)
 class NodeControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
 
-    @Test
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
     void importNodes() throws Exception {
         String request = """
 				   {
